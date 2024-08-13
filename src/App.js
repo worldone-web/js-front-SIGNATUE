19de9bbc1a55446797a339ccd3dddae93ce90598
Jae-Young-Kim <tjfansghk@naver.com>
2024-08-13 21:58:00 +0900

    App.js update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import { Component } from "./core/heropy";
 export default class App extends Component{
     constructor(){
         super({
-            tagName:'h1'
+            state: {
+                inputText:''
+            }
         })
     }
+    
     render(){
-        this.el.textContent="Hello, world!"
+        this.el.classList.add('search')
+        this.el.innerHTML=`
+        <input />
+        <button>Click!</button>
+        `
+
+
+        const inputEl=this.el.querySelector('input') //App.js에서 input 요소를 찾음
+        inputEl.addEventListener('input',()=>{
+            this.state.inputText=inputEl.value
+        })
+
+        const buttonEl = this.el.querySelector('button')
+        buttonEl.addEventListener('click',()=>{
+            console.log(this.state.inputText)
+        })
     }
 }
--- src/core/heropy.js
+++ src/core/heropy.js
@@ -1,8 +1,12 @@
 /////// Component ////////
 export class Component{
     constructor(payload = {} ){ // 태그 생성
-        const { tagName = 'div' }=payload // 기본값은 div
+        const { 
+            tagName = 'div', 
+            state= {} 
+        } = payload // 기본값은 div
         this.el = document.createElement(tagName)
+        this.state = state
         this.render()
     }
     render(){ // 컴포넌트를 확장해서 사용할 떄 사용(자식 컴포넌트가 함수 재정의) - 출력 역할
